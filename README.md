a48ea1d5f0bdc070be78fdca3c0ad33f227fc738
Savage Lasa <superoutput@gmail.com>
2021-04-13 23:55:12 +0700

    First create REST API in Rust language using Actix framework

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # rust-connection-pool-api
 Rust project for database connection pool maintaining a number of open connections to a database and providing in a Functions as a Service (FaaS) model to build applications deployed as “serverless” chunks of code that run completely on the cloud provider’s platform computing infrastructure. With a FaaS platform, you don’t have to manage any server infrastructure and only pay for the computing cycles required to execute the function.
+
+## APIs
+- POST `/api/files/` Upload a file
+- GET `/api/files/{name}/` Download a file with a name
+
+## Credit
+
+The examples on this publication are guided by [Build Your First REST API in Rust Language Using Actix Framework](https://medium.com/swlh/build-your-first-rest-api-using-rust-language-and-actix-framework-8f827175b30f) by **Mehrdad Esmaeilpour** in **Medium.com**.
+
+Rust Document Guide:
+
+[Rust by Example](https://doc.rust-lang.org/rust-by-example/index.html)
+###Crate [std](https://doc.rust-lang.org/std/index.html)
+[Functional Programming in Rust](https://mmstick.gitbooks.io/rust-programming-phoronix-reader-how-to/content/chapter02.html)
+
+Other references:
+
+- [Web Development with Rust — 03/x: Create a REST API](https://dev.to/gruberb/web-development-with-rust-03-x-create-a-rest-api-3i82) by **Bastian Gruber**
+- [Creating a REST API in Rust with warp](https://blog.logrocket.com/creating-a-rest-api-in-rust-with-warp/) by **Bastian Gruber**
+- [How to Build a REST API in Rust — A Step-by-Step Guide](https://betterprogramming.pub/rest-api-in-rust-step-by-step-guide-b8a6c5fcbff0) by **Asel Siriwardena**
+- [Swagger Pet Store with Rust](https://medium.com/@pvinchon/swagger-pet-store-with-rust-8a8846868b42) by **Philippe**
---
+++ src/main.rs
@@ -0,0 +1,114 @@
+use actix_multipart::Multipart;
+use actix_web::{get, post, web, App, Error, HttpResponse, HttpServer, Responder};
+use futures::stream::once;
+use futures::future::ok;
+use futures::StreamExt;
+use serde::{Serialize, Deserialize};
+use std::time::{SystemTime, UNIX_EPOCH};
+use std::io::Write;
+
+const UPLOAD_PATH: &str = "../storage/upload";
+
+#[derive(Serialize, Deserialize)]
+struct Health {
+    status: String,
+    total_space: i32,
+    free_space: i32
+}
+
+#[derive(Serialize, Deserialize)]
+struct File {
+    name: String,
+    time: u64,
+    err: String
+}
+
+#[derive(Deserialize)]
+struct Download {
+    name: String
+}
+
+#[get("/")]
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hi! I am R00† .")
+}
+
+#[get("/health/")]
+async fn health() -> impl Responder {
+    let u = &Health {
+        status: "UP".to_string(),
+        total_space: 0,
+        free_space: 0
+    };
+    HttpResponse::Ok().json(u)
+}
+
+#[post("/files/")]
+async fn upload(mut payload: Multipart) -> Result<HttpResponse, Error> {
+    std::fs::create_dir_all(UPLOAD_PATH)?;
+    let mut filename = "".to_string();
+    while let Some(Ok(mut field)) = payload.next().await {
+        let content_type = field.content_disposition().unwrap();
+        filename = format!("{} - {}", SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_micros(), content_type.get_filename().unwrap(), );
+        let filepath = format!("{}/{}", UPLOAD_PATH, sanitize_filename::sanitize(&filename));
+        // File::create is blocking operation, use thread pool
+        let mut f = web::block(|| std::fs::File::create(filepath))
+            .await
+            .unwrap();
+        // Field in turn is stream of *Bytes* object
+        while let Some(chunk) = field.next().await {
+            let data = chunk.unwrap();
+            // filesystem operations are blocking, use thread pool
+            f = web::block(move || f.write_all(&data).map(|_| f)).await?;
+        }
+    }
+    // Result::<_, std::io::Error>::Ok(res)
+    // or
+    // std::io::Result::Ok(res)
+    Ok(HttpResponse::Ok().json(&File {
+        name: filename,
+        time: SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs(),
+        err: "".to_string(),
+    }))
+}
+
+// async fn test(field: Field) {
+//     let mut filename = "".to_string();
+//     println!("{:?}", field);
+//     let content_type = field.content_disposition().unwrap();
+//     filename = format!("{} - {}", SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_micros(), content_type.get_filename().unwrap, );
+//     let filepath = format!("{}/{}", UPLOAD_PATH, sanitize_filename::sanitize(&filename));
+//     let mut f = web::block(|| std::fs::File::create(filepath))
+//         .await
+//         .unwrap();
+//     while let Some(chunk) = field.next().await {
+//         let data = chunk.unwrap();
+//         f = web::block(move || f.write_all(&data).map(|_| f)).await;
+//     }
+// }
+
+#[get("/files/{name}/")]
+async fn download(info: web::Path<Download>) -> HttpResponse {
+    let name = String::from(info.name.as_str());
+    let body = once(ok::<_, Error>(web::Bytes::from(name)));
+
+    HttpResponse::Ok()
+        .content_type("application/json")
+        .streaming(body)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(web::scope("/api")
+                .service(index)
+                .service(health)
+                .service(upload)
+                .service(download)
+            )
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
+}
